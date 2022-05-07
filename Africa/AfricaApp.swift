c25f0e9a452dbfa0857169ce05ef17d56a75dd04
Edney Mesquita <edneyphp@outlook.com>
2022-05-06 23:48:40 -0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Edney Mesquita on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
