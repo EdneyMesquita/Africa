bcd94253cc85fb3b6ce6da322be05f7f66ed22b0
Edney Mesquita <edneyphp@outlook.com>
2022-05-07 00:45:00 -0300

    feat: create new json decoder and the cover image component

---
+++ Africa/Models/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Edney Mesquita on 07/05/22.
+//
+
+import Foundation
+
+struct CoverImage: Identifiable, Codable {
+    let id: Int
+    let name: String
+}
+
